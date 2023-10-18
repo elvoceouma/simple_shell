1ff262d3f14f5b7e5470183224226dd96a1c4ec4
jen-ny-k <99100881+jen-ny-k@users.noreply.github.com>
2023-10-17 17:48:53 -0700

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -102,13 +102,13 @@ int _myalias(info_t *info)
 		}
 		return (0);
 	}
-	for (i = 1; info->argv[i]; i++)
+	for (j = 1; info->argv[j]; j++)
 	{
-		p = _strchr(info->argv[i], '=');
+		p = _strchr(info->argv[j], '=');
 		if (p)
-			set_alias(info, info->argv[i]);
+			set_alias(info, info->argv[j]);
 		else
-			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+			print_alias(node_starts_with(info->alias, info->argv[j], '='));
 	}
 
 	return (0);
